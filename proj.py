d078a6b82a366897289eb5358e1df7fad91698a7
Kharamot <jaggression@gmail.com>
2017-04-10 11:19:05 -0700

    What is going on?

--- proj.py
+++ proj.py
@@ -1,3 +1,40 @@
-from pyeda import *
+from pyeda.boolalg.bdd import (
+    bddvar,  expr2bdd, bdd2expr
+)
+     
+from pyeda.boolalg.expr import exprvar
 
-print("hello world")
+x1, x2, y1, y2, z1, z2 = map(exprvar, 'abcdef')
+xx1, xx2, yy1, yy2, zz1, zz2 = map(bddvar, 'abcdef')
+
+#translating edges to bool formulas and bdds
+
+#first edge 0-->1, i.e., 00 --> 01
+r= ~x1 & ~x2 & ~y1 & y2
+rr= expr2bdd(r)
+
+#second edge 1-->2; i.e., 01-->10
+r= ( ~x1 & x2 & y1 & ~y2  )
+rr= rr | expr2bdd(r)
+
+#third edge  2-->3; i.e., 10-->11
+r= ( x1 & ~x2 & y1 & y2  )
+rr= rr | expr2bdd(r)
+
+#fourth edge  3-->0; i.e., 11-->00
+r= ( x1 & x2 & ~y1 & ~y2  )
+rr= rr | expr2bdd(r)
+
+
+#two step reachability
+hh=rr.compose({yy1:zz1, yy2:zz2}) & rr.compose({xx1:zz1, xx2:zz2})
+print(hh)
+hh=hh.smoothing({zz1,zz2})
+
+#test node 3 can reach node 0 in two steps, which must be false
+#assert hh.restrict({xx1:1, xx2:1, yy1:0, yy2:0})
+
+
+
+
+exit(0)
